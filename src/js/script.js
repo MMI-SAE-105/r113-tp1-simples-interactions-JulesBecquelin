9436c640e6098367e1e60f69d7e0134879538d79
JulesBecquelin <jules.becquelin@edu.univ-fcomte.fr>
2024-11-08 11:13:16 +0100

    ajout du texte en rouge, réaction à un click sur un button et les titres "h2"

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
 // TODO
+const aMettreEnRouge = document.querySelector("#a-mettre-en-rouge")
+aMettreEnRouge.style.color="red";
+
+const enRougSuiteAClick = document.querySelector("#en-rouge-suite-a-click")
+enRougSuiteAClick.addEventListener("click",(elm)=>{
+    enRougSuiteAClick.style.color = "red"
+});
+
+const collH2 = document.querySelectorAll("h2")
+collH2.forEach((elm)=>{
+    elm.addEventListener("click",(evt)=>{
+evt.target.style.color = "red"
+    })
+});
